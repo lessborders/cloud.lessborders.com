ab68b66a44054cbb1b493a2dde8d5e33bde2d126
Billie Bell <billie.bell@net-up.de>
2022-03-20 00:19:40 +0100

    fixes

--- src/pages/Auth/Login.vue
+++ src/pages/Auth/Login.vue
@@ -6,26 +6,31 @@
                 in <strong class="text-gradient">one</strong> place
             </div>
         </div>
-        <div class="col-12 col-lg-6 col-xl-5 text-center p-3 card-bg d-flex">
+        <div class="col-12 col-lg-6 col-xl-5 text-center p-3 card-bg d-flex position-relative">
+            <div class="d-flex position-absolute p-3" style="top:0;left:0;right:0;">  
+                <darkToggle class="ms-auto"/>
+            </div>
             <div class="small-container m-auto col pt-3 pb-5">
                 <img class="invert-dark logo my-3" height="48" width="48" alt="Less Borders Logo" src="https://static.lessborders.com/apps/lessborders/lessborders_logo.svg"/>
                 <br>
 
                 <h5>Sign in</h5>
                 <p>With your <span class="text-gradient">Cloud</span> account</p>
+                <form v-on:submit.prevent="login">
+                        
+                    <div class="form-floating mb-2">
+                        <input v-model="email" name="email" type="email" class="form-control" placeholder="Email"/>
+                        <label for="email">Email</label>
+                    </div>
 
-                <div class="form-floating mb-2">
-                    <input v-model="email" name="email" type="email" class="form-control" placeholder="Email"/>
-                    <label for="email">Email</label>
-                </div>
-
-                <div class="form-floating mb-2">
-                    <input v-model="password" name="password" type="password" class="form-control" placeholder="Password"/>
-                    <label for="password">Password</label>
-                </div>
-                <div class="mt-3 text-end">
-                    <button @click="login" class="btn btn-primary">Sign in <i class="fa-regular fa-angle-right ms-1"></i></button>
-                </div>
+                    <div class="form-floating mb-2">
+                        <input v-model="password" name="password" type="password" class="form-control" placeholder="Password"/>
+                        <label for="password">Password</label>
+                    </div>
+                    <div class="mt-3 text-end">
+                        <button type="submit" class="btn btn-primary">Sign in <i class="fa-regular fa-angle-right ms-1"></i></button>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
