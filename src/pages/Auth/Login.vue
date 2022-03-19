3adea038e0d991ab98df42765b5aa4914db1c7a6
Billie Bell <billie.bell@net-up.de>
2022-03-19 10:28:23 +0100

    new stuff

---
+++ src/pages/Auth/Login.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="page-container mb-auto">
+      hi
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Sign in"
+  };
+</script>
---
+++ src/pages/Auth/Register.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="page-container mb-auto">
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Sign up"
+  };
+</script>
